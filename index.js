ad11ea6f9a5624abc065956e89aa2c919819113a
John D <johnd@Johns-MBP.attlocal.net>
2024-01-23 22:25:15 -0600

    inquirer install

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-const generateMarkdownSVGimage.js = require("./SVGimage")
+
+const SVGimage = require("./SVGimage")
 const inquirer = require("inquirer")
+const fs = require("fs")
+
+
+
+
+const questions = [
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color (keyword or hexadecimal):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color (keyword or hexadecimal):',
+    },
+]
+
+
+function init() {
+    inquirer.prompt(questions).then(data => {
+        console.log(data)
+        fs.writeFileSync("logo.svg", generateMarkdown(data))
+    })
+}
+ init()
+
