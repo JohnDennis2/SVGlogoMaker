e820620bd838f35bb5637fbd4d8b29e96e98b691
John D <johnd@Johns-MBP.attlocal.net>
2024-01-24 18:15:14 -0600

    shapes and generators working

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
+const init = require("./lib/SVGgenerator")
 
-const inquirer = require('inquirer');
-const generateSVG = require('./lib/SVGimage');
-
-async function run() {
-  const userInput = await promptUser();
-  generateSVG(userInput);
-}
-
-const questions = [
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter up to three characters:',
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter text color (keyword or hexadecimal):',
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Choose a shape:',
-      choices: ['circle', 'triangle', 'square'],
-    },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter shape color (keyword or hexadecimal):',
-    },
-]
-
-
-function init() {
-    inquirer.prompt(questions).then(data => {
-        console.log(data)
-        fs.writeFileSync("logo.svg", generateMarkdown(data))
-    })
-}
  init()
 
+
+// const inquirer = require('inquirer');
+// const generateSVG = require('./lib/SVGimage');
+
+// async function run() {
+//   const userInput = await promptUser();
+//   generateSVG(userInput);
+// }
+
+// const questions = [
+//     {
+//       type: 'input',
+//       name: 'text',
+//       message: 'Enter up to three characters:',
+//     },
+//     {
+//       type: 'input',
+//       name: 'textColor',
+//       message: 'Enter text color (keyword or hexadecimal):',
+//     },
+//     {
+//       type: 'list',
+//       name: 'shape',
+//       message: 'Choose a shape:',
+//       choices: ['circle', 'triangle', 'square'],
+//     },
+//     {
+//       type: 'input',
+//       name: 'shapeColor',
+//       message: 'Enter shape color (keyword or hexadecimal):',
+//     },
+// ]
+
+
+// function init() {
+//     inquirer.prompt(questions).then(data => {
+//         console.log(data)
+//         fs.writeFileSync("logo.svg", generateMarkdown(data))
+//     })
+// }
